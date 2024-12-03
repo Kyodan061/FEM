339711c93548aeace2e2e0ba9cf9ecb35691b5a4
Kyodan061 <120133157+Kyodan061@users.noreply.github.com>
2024-12-03 12:58:39 -0300

    Inicial

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const FEM = {};
+
+/**
+ * The set of Ability Scores used within the system.
+ * @type {Object}
+ */
+FEM.abilities = {
+  str: 'FEM.Ability.Str.long',
+  dex: 'FEM.Ability.Dex.long',
+  con: 'FEM.Ability.Con.long',
+  int: 'FEM.Ability.Int.long',
+  wis: 'FEM.Ability.Wis.long',
+  cha: 'FEM.Ability.Cha.long',
+};
+
+FEM.abilityAbbreviations = {
+  str: 'FEM.Ability.Str.abbr',
+  dex: 'FEM.Ability.Dex.abbr',
+  con: 'FEM.Ability.Con.abbr',
+  int: 'FEM.Ability.Int.abbr',
+  wis: 'FEM.Ability.Wis.abbr',
+  cha: 'FEM.Ability.Cha.abbr',
+};
